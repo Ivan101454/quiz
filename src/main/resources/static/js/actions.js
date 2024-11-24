2cedf7a9d259e25206b7d4e1bdff6c9c62479215
Ivan Sapozhnikov <ivan101454@gmail.com>
2024-11-24 15:35:00 +0300

    add view with javascript

--- src/main/resources/static/js/actions.js
+++ src/main/resources/static/js/actions.js
@@ -1,3 +1,12 @@
-function showAnswer(result) {
-    alert("Правильный ответ: " + result);
-}
+// let answer_right = [[${card.getRightAnswer()}]];
+// let value = document.querySelector('input[name="answer"]:checked').value;
+// let result = "default";
+// if(value === answer_right) {
+//     result = "Это правильный ответ";
+// } else {
+//     result = "Это неверный ответ";
+// }
+// function showAnswer() {
+//     // alert("Правильный ответ: " + result);
+//     document.getElementById("showtext").textContent = result;
+// }
